641529233e456e9b1bbd7e3def5d5c6340cb7f1a
LucaSinUnaS <145080298+LucaSinUnaS@users.noreply.github.com>
2023-11-07 01:11:04 -0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 	$db_host = "localhost";
 	$db_user = "u956478100_ztech";
-	$db_password = "Superlol2112";
-	$db_name = "u956478100_quehayhoy";
+	$db_password = "xxx";
+	$db_name = "xxx";
 
 	try{
 		$db = new PDO("mysql:host={$db_host};db_name={$db_name}", $db_user, $db_password);
